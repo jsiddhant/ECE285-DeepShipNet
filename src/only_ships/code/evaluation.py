f4460a284b96bb51eaf1dd96d78ba684ab2acfc5
Albrecht Wigand <37198703+a-wigand@users.noreply.github.com>
2018-12-14 14:37:06 -0800

    Add files via upload

---
+++ src/only_ships/code/evaluation.py
@@ -0,0 +1,133 @@
+import numpy as np
+
+from skimage.data import imread
+from skimage.morphology import label
+from src.pipeline import rle_to_mask
+
+def calc_IoU(A, B):
+    AorB = np.logical_or(A,B).astype('int')
+    AandB = np.logical_and(A,B).astype('int')
+    IoU = AandB.sum() * 1.0 / AorB.sum()
+    return IoU
+
+def calc_IoU_vector(A, B):
+    score_vector = []
+    IoU = calc_IoU(A, B)
+    for threshold in np.arange(0.5,1,0.05):
+        score = int(IoU > threshold)
+        score_vector.append(score)
+    return score_vector
+
+def calc_IoU_tensor(masks_true, masks_pred):
+    true_mask_num = masks_true.shape[0]
+    pred_mask_num = masks_pred.shape[0]
+    score_tensor = np.zeros((true_mask_num, pred_mask_num, 10))
+    for true_i in range(true_mask_num):
+        for pred_i in range(pred_mask_num):
+            true_mask = masks_true[true_i]
+            pred_mask = masks_pred[pred_i]
+            score_vector = calc_IoU_vector(true_mask, pred_mask)
+            score_tensor[true_i,pred_i,:] = score_vector
+    return score_tensor
+
+def calc_F2_per_one_threshold(score_matrix):
+    tp = np.sum( score_matrix.sum(axis=1) > 0  )
+    fn = np.sum( score_matrix.sum(axis=1) == 0 )
+    fp = np.sum( score_matrix.sum(axis=0) == 0 )
+    F2 = (5*tp) * 1.0 / ((5*tp) + fp + (4*fn))
+    return F2
+
+def calc_score_one_image(mask_true, mask_pred):
+    mask_true = mask_true.reshape(768,768)
+    mask_pred = mask_pred.reshape(768,768)
+    if mask_true.sum() == 0 and mask_pred.sum() == 0:
+        score = 1
+    elif mask_true.sum() == 0 and mask_pred.sum() != 0:
+        score = 0
+    elif mask_true.sum() != 0 and mask_pred.sum() == 0:
+        score = 0
+    else:
+        mask_label_true = label(mask_true)
+        mask_label_pred = label(mask_pred)
+        c_true = np.max(mask_label_true)
+        c_pred = np.max(mask_label_pred)
+        tmp = []
+        for k in range(c_true):
+            tmp.append(mask_label_true == k+1)
+        masks_true = np.stack(tmp, axis=0)
+        tmp = []
+        for k in range(c_pred):
+            tmp.append(mask_label_pred == k+1)
+        masks_pred = np.stack(tmp, axis=0)
+        score_tensor = calc_IoU_tensor(masks_true, masks_pred)
+        F2_t = []
+        for i in range(10):
+            F2 = calc_F2_per_one_threshold(score_tensor[:,:,i])
+            F2_t.append(F2)
+        score = np.mean(F2_t)
+    return score
+
+def calc_score_all_image(batch_mask_true, batch_mask_pred, threshold=0.5):
+    num = batch_mask_true.shape[0]
+    tmp = batch_mask_pred > threshold
+    batch_mask_pred = tmp.astype('int')
+    scores = list()
+    for i in range(num):
+        score = calc_score_one_image(batch_mask_true[i], batch_mask_pred[i])
+        scores.append(score)
+    return np.mean(scores)
+
+def create_data(image_list, train_img_dir, train_df):
+    batch_img = []
+    batch_mask = []
+    for name in image_list:
+        tmp_img = imread(train_img_dir + name)
+        batch_img.append(tmp_img)
+        mask_list = train_df['EncodedPixels'][train_df['ImageId'] == name].tolist()
+        one_mask = np.zeros((768, 768, 1))
+        for item in mask_list:
+            rle_list = str(item).split()
+            tmp_mask = rle_to_mask(rle_list, (768, 768))
+            one_mask[:,:,0] += tmp_mask
+        batch_mask.append(one_mask)
+    img = np.stack(batch_img, axis=0)
+    mask = np.stack(batch_mask, axis=0)
+    img = img / 255.0
+    mask = mask / 255.0
+    return img, mask
+
+def calc_score_all_image(batch_mask_true, batch_mask_pred, threshold=0.5):
+    num = batch_mask_true.shape[0]
+    tmp = batch_mask_pred > threshold
+    batch_mask_pred = tmp.astype('int')
+    scores = list()
+    for i in range(num):
+        score = calc_score_one_image(batch_mask_true[i], batch_mask_pred[i])
+        scores.append(score)
+    return np.mean(scores)
+
+def multi_rle_encode(img, **kwargs):
+    '''
+    Encode connected regions as separated masks
+    '''
+    labels = label(img[0,:,:,:])
+    if img.ndim > 2:
+        return [rle_encode(np.sum(labels==k, axis=2), **kwargs) for k in np.unique(labels[labels>0])]
+    else:
+        return [rle_encode(labels==k, **kwargs) for k in np.unique(labels[labels>0])]
+
+# ref: https://www.kaggle.com/paulorzp/run-length-encode-and-decode
+def rle_encode(img, min_max_threshold=1e-3, max_mean_threshold=None):
+    '''
+    img: numpy array, 1 - mask, 0 - background
+    Returns run length as string formated
+    '''
+    if np.max(img) < min_max_threshold:
+        return '' ## no need to encode if it's all zeros
+    if max_mean_threshold and np.mean(img) > max_mean_threshold:
+        return '' ## ignore overfilled mask
+    pixels = img.T.flatten()
+    pixels = np.concatenate([[0], pixels, [0]])
+    runs = np.where(pixels[1:] != pixels[:-1])[0] + 1
+    runs[1::2] -= runs[::2]
+    return ' '.join(str(x) for x in runs)
